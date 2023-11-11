cd9e3a536acceef24bdd5eb79c116c67003d0529
K0ppai <psthu.koppai@gmail.com>
2023-11-11 23:52:18 +0630

    Populate Main Page data

--- app/restaurant/[slug]/components/RestaurantNavBar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
-const RestaurantNavBar = () => {
+const RestaurantNavBar = ({ slug }: { slug: string }) => {
   return (
     <nav className="flex text-reg border-b pb-2">
-      <Link href="/restaurant/1" className="mr-7">
+      <Link href={`/restaurant/${slug}`} className="mr-7">
         Overview
       </Link>
-      <Link href="/restaurant/1/menu" className="mr-7">
+      <Link href={`/restaurant/${slug}/menu`} className="mr-7">
         Menu
       </Link>
     </nav>
   );
-}
+};
 
-export default RestaurantNavBar
+export default RestaurantNavBar;
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,4 @@
-import NavBar from '@/app/components/NavBar';
 import React from 'react';
-import Header from './components/Header';
 import RestaurantNavBar from './components/RestaurantNavBar';
 import ReservationCard from './components/ReservationCard';
 import Reviews from './components/Reviews';
@@ -8,16 +6,50 @@ import Images from './components/Images';
 import Description from './components/Description';
 import Rating from './components/Rating';
 import Title from './components/Title';
+import { PrismaClient } from '@prisma/client';
 
-const DetailPage = () => {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  slug: string;
+  name: string;
+  images: string[];
+  description: string;
+}
+
+const fetchRestaurant = async (slug: string):Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+
+  return restaurant;
+};
+
+const DetailPage = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurant(params.slug);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug}/>
+        <Title name={restaurant.name}/>
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description}/>
+        <Images images={restaurant.images}/>
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
