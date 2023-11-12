1cbfef85d95d5a53cdc69ee37e786b8d3cffaeba
K0ppai <psthu.koppai@gmail.com>
2023-11-12 21:28:50 +0630

    Update menu page for dynamic rendering

--- app/restaurant/[slug]/menu/components/Menu.tsx
+++ app/restaurant/[slug]/menu/components/Menu.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import MenuCard from './MenuCard';
+import { Item } from '@prisma/client';
 
-const Menu = () => {
+const Menu = ({ items }: { items: Item[] }) => {
   return (
     <main className="bg-white mt-5">
       <div>
@@ -9,9 +10,13 @@ const Menu = () => {
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
         <div className="flex flex-wrap justify-between">
-          {/* MENU CARD */}
-          <MenuCard />
-          {/* MENU CARD */}
+          {items.length === 0 ? (
+            <p>There is no menu yet in this restaurant.</p>
+          ) : (
+            items.map((item) => {
+              return <MenuCard key={item.id} item={item} />;
+            })
+          )}
         </div>
       </div>
     </main>
