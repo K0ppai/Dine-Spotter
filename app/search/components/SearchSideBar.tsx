c7073089fd4e596ce2553d0d99cd9338fca24020
K0ppai <psthu.koppai@gmail.com>
2023-11-12 21:26:44 +0630

    Populate locations and cuisines in sidebar

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,35 +1,39 @@
-import React from 'react'
+import { Cuisine, Location } from '@prisma/client';
+import React from 'react';
 
-const SearchSideBar = () => {
+const SearchSideBar = async ({
+  locations,
+  cuisines,
+}: {
+  locations: Location[];
+  cuisines: Cuisine[];
+}) => {
   return (
     <div className="w-1/5">
       <div className="border-b pb-4">
         <h1 className="mb-2">Region</h1>
-        <p className="font-light text-reg">Toronto</p>
-        <p className="font-light text-reg">Ottawa</p>
-        <p className="font-light text-reg">Montreal</p>
-        <p className="font-light text-reg">Hamilton</p>
-        <p className="font-light text-reg">Kingston</p>
-        <p className="font-light text-reg">Niagara</p>
+        {locations.map((location) => (
+          <p key={location.id} className="font-light capitalize">{location.name}</p>
+        ))}
       </div>
       <div className="border-b pb-4 mt-3">
         <h1 className="mb-2">Cuisine</h1>
-        <p className="font-light text-reg">Mexican</p>
-        <p className="font-light text-reg">Italian</p>
-        <p className="font-light text-reg">Chinese</p>
+        {cuisines.map((cuisine) => (
+          <p key={cuisine.id} className="font-light capitalize">{cuisine.name}</p>
+        ))}
       </div>
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
         <div className="flex">
-          <button className="border w-full text-reg font-light rounded-l p-2">$</button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2">$$</button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
+          <button className="border w-full text-[15px] font-light rounded-l p-2">$</button>
+          <button className="border-r border-t border-b w-full text-[15px] font-light p-2">$$</button>
+          <button className="border-r border-t border-b w-full text-[15px] font-light p-2 rounded-r">
             $$$
           </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default SearchSideBar
+export default SearchSideBar;
