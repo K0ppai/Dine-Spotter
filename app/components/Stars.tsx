fa2824b85bdd9cbb85514c5ee76f74c6953aad5d
K0ppai <psthu.koppai@gmail.com>
2023-11-15 23:23:44 +0630

    Create a stars component

---
+++ app/components/Stars.tsx
@@ -0,0 +1,44 @@
+import { calculateAverageRating } from '@/utils/calculateAverageRating';
+import { Review } from '@prisma/client';
+import React from 'react';
+import fullstar from '../../public/icons/full-star.png';
+import halfstar from '../../public/icons/half-star.png';
+import emptystar from '../../public/icons/empty-star.png';
+import Image from 'next/image';
+
+const Stars = ({ reviews }: { reviews: Review[] }) => {
+  const rating = calculateAverageRating(reviews);
+
+  const renderStars = () => {
+    const stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+      const difference = parseFloat((rating - i).toFixed(1));
+      if (difference >= 1) {
+        stars.push(fullstar);
+      } else if (difference > 0 && difference < 1) {
+        if (difference <= 0.2) {
+          stars.push(emptystar);
+        } else if (difference > 0.2 && difference <= 0.6) {
+          stars.push(halfstar);
+        } else {
+          stars.push(fullstar);
+        }
+      } else {
+        stars.push(emptystar);
+      }
+    }
+
+    return stars.map((star, i) => {
+      return <Image src={star} alt="" className="w-4 h-4 mr-1" key={i} />;
+    });
+  };
+
+  return (
+    <>
+      <div className="flex items-center">{renderStars()}</div>
+    </>
+  );
+};
+
+export default Stars;
