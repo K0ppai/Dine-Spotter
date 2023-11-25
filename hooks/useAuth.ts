4f36042b841a9a88e56ce434f56ad17ea8636dd2
K0ppai <psthu.koppai@gmail.com>
2023-11-25 22:06:42 +0630

    Create a custom useAuth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const useAuth = () => {
+  const signin = async ({email, password} : {email: string, password: string}) => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/signin', {
+        email,
+        password
+      });
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const signout = () => {
+    console.log('signout')
+  }
+
+  return {
+    signin,
+    signout
+  }
+}
+
+export default useAuth
