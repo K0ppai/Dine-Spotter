5539754a4a5122bfbf859896017e0e626258a3df
K0ppai <psthu.koppai@gmail.com>
2023-11-26 18:04:39 +0630

    Update signin for loading in useAuth hook

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,25 +1,37 @@
-import axios from 'axios'
+import { AuthenticationContext } from '@/app/context/AuthContext';
+import axios from 'axios';
+import { useContext } from 'react';
 
 const useAuth = () => {
-  const signin = async ({email, password} : {email: string, password: string}) => {
+  const { data, error, loading, setAuthState } = useContext(AuthenticationContext);
+
+  const signin = async ({ email, password }: { email: string; password: string }) => {
+    setAuthState((prev) => ({ ...prev, loading: true }));
+
     try {
       const response = await axios.post('http://localhost:3000/api/auth/signin', {
         email,
-        password
+        password,
       });
-      console.log(response)
-    } catch (error) {
-      console.log(error)
+
+      setAuthState({ data: response.data, loading: false, error: null });
+    } catch (error: any) {
+      setAuthState((prev) => ({
+        ...prev,
+        error: error.response.data.errorMessage,
+        loading: false,
+      }));
+      console.log(error);
     }
-  }
+  };
   const signout = () => {
-    console.log('signout')
-  }
+    console.log('signout');
+  };
 
   return {
     signin,
-    signout
-  }
-}
+    signout,
+  };
+};
 
-export default useAuth
+export default useAuth;
