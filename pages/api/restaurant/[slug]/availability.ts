c517df2e12f6c1c5bb4a9feaa9f6fc47dec3b621
K0ppai <psthu.koppai@gmail.com>
2023-12-03 22:43:07 +0630

    Update availability end point for fetching bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { times } from '../../../../data';
+import { PrismaClient } from '@prisma/client';
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const prisma = new PrismaClient();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug, day, time, partySize } = req.query as {
     slug: string;
     day: string;
@@ -9,10 +13,30 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
   };
 
   if (!day || !time || !partySize) {
-    return res.status(400).json({ errorMessage: 'Missing query parameters' });
+    return res.status(400).json({ errorMessage: 'Invalid data provided' });
   }
 
-  return res.json({ slug, day, time, partySize });
+  const searchTime = times.find((t) => t.time === time)?.searchTimes;
+
+  if (!searchTime) {
+    return res.status(400).json({ errorMessage: 'Invalid data provided' });
+  }
+
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTime[0]}`),
+        lte: new Date(`${day}T${searchTime[searchTime.length - 1]}`),
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  return res.json({ searchTime, bookings });
 };
 
 export default handler;
