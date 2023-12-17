4b2e58a9b742cfee5837e74f4f64e660468c2b50
K0ppai <psthu.koppai@gmail.com>
2023-12-17 20:43:37 +0630

    Create useAvailabilities hook

---
+++ hooks/useAvailabilities.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export const useAvailabilities = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchAvailabilities = async ({
+    slug,
+    day,
+    time,
+    partySize,
+  }: {
+    slug: string;
+    day: string;
+    time: string;
+    partySize: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/restaurant/${slug}/availability`,
+        {
+          params: {
+            day,
+            time,
+            partySize,
+          },
+        },
+      );
+      setData(response.data);
+      setLoading(false);
+    } catch (responseError: any) {
+      setError(responseError.response.data.errorMessages);
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    error,
+    loading,
+    fetchAvailabilities,
+  };
+};
