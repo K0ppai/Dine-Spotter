880e2d73da50aaf97efecb6451a7a4d1ad1c6cae
K0ppai <psthu.koppai@gmail.com>
2023-12-20 14:24:00 +0630

    Render dynamically according to the reserve form

--- app/reserve/[slug]/components/Header.tsx
+++ app/reserve/[slug]/components/Header.tsx
@@ -1,21 +1,31 @@
+import { convertToDisplayTime } from '@/utils/convertToDisplayTime';
+import { format } from 'date-fns';
 import React from 'react';
 
-const Header = () => {
+const Header = ({
+  name,
+  image,
+  partySize,
+  date,
+}: {
+  name: string;
+  image: string;
+  partySize: string;
+  date: string;
+}) => {
+  const [day, time] = date.split('T');
+
   return (
     <div>
       <h3 className="font-bold">You're almost done!</h3>
       <div className="mt-5 flex">
-        <img
-          src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-          alt=""
-          className="w-32 h-18 rounded"
-        />
+        <img src={image} alt="" className="w-32 h-18 rounded" />
         <div className="ml-4">
-          <h1 className="text-3xl font-bold">Aiāna Restaurant Collective</h1>
+          <h1 className="text-3xl font-bold">{name}</h1>
           <div className="flex mt-3">
-            <p className="mr-6">Tues, 22, 2023</p>
-            <p className="mr-6">7:30 PM</p>
-            <p className="mr-6">3 people</p>
+            <p className="mr-6">{format(new Date(day), 'iii d, MMM, y')}</p>
+            <p className="mr-6">{convertToDisplayTime(time)}</p>
+            <p className="mr-6">{partySize === '1' ? '1 person' : `${partySize} people`}</p>
           </div>
         </div>
       </div>
